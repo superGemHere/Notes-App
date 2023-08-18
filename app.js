608882765e1cf8f7f07d6833af6de1d1c4659e69
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-08-18 16:49:58 +0300

    Functionalities RDY
    Left only styling.

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
+import {html, render} from "./node_modules/lit-html/lit-html.js";
+
 const enterBtn = document.querySelector('.enterBtn');
 const parentDiv = document.querySelector('.notesDiv');
 const input = document.querySelector('.content');
 
 enterBtn.addEventListener('click', createNote);
 
-
-
+const createTemplate= () => html`<p class="quotes">${input.textContent} <button @click="${deleteNote}" class="deleteBtn"><img src="./images/icons8-x-50.png" height="25px" alt="del icon"></button></p>`;
 
 function createNote(){
-    let divEl = document.createElement('div');
-    divEl.style.width="350px" ;
-    divEl.style.height="fit-content"; 
-    divEl.innerHTML = `<p class="quotes">${input.textContent} <button class="deleteBtn"><img src="./images/icons8-x-50.png" height="25px" alt="del icon"></button></p>`;
-    parentDiv.appendChild(divEl);
+    let div = document.createElement('div');
+    div.style.width = "350px";
+    div.style.height = "fit-content";
+    render(createTemplate(), div);
+
+    parentDiv.appendChild(div);
+    
 }
+
+function deleteNote(e){
+    const target = e.target.parentNode.parentNode;
+    console.log(target);
+    target.remove();
+    
+}
+
+
+
+
